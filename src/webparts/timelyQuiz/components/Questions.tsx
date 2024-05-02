422a58ce4139a1171eaaf24ec9266df19136e7b3
Arief Iman Santoso <ariefimansantoso@gmail.com>
2024-05-02 16:43:46 +0700

    handle next question

--- src/webparts/timelyQuiz/components/Questions.tsx
+++ src/webparts/timelyQuiz/components/Questions.tsx
@@ -1,12 +1,37 @@
 import * as React from "react";
 import styles from './Questions.module.scss';
+import { mergeStyleSets, DefaultButton, FocusTrapZone, Layer, Overlay, Popup } from '@fluentui/react';
+import { useBoolean } from '@fluentui/react-hooks';
 
 export interface IQuestionsDetail {
     TotalScore: number,
     CurrentQuestion: number
   }
 
+const popupStyles = mergeStyleSets({
+    root: {
+        background: 'rgba(0, 0, 0, 0.2)',
+        bottom: '0',
+        left: '0',
+        position: 'fixed',
+        right: '0',
+        top: '0',
+    },
+    content: {
+        background: 'white',
+        left: '50%',
+        maxWidth: '400px',
+        padding: '0 2em 2em',
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+    },
+});
+
 export default class Questions extends React.Component<{}, IQuestionsDetail> {
+
+    private inputName: string = "option-99";
+    const [isPopupVisible, { setTrue: showPopup, setFalse: hidePopup }] = useBoolean(false);
 
     constructor(props: any, state: IQuestionsDetail) {
         super(props);
@@ -43,10 +68,10 @@ export default class Questions extends React.Component<{}, IQuestionsDetail> {
             </div>
 
             <div className={`${styles.nextButtonContainer}`}>
-                <button>Next Question</button>
+                <button onClick={() => this.renderNextQuestion(theQuestion)}>Next Question</button>
             </div>
 
-        </div>
+        </div>        
         );
     }
 
@@ -57,9 +82,10 @@ export default class Questions extends React.Component<{}, IQuestionsDetail> {
             let questionOptionElements = theQuestion.options.map((o: string, key: number) => {
                 let labelId = "option-" + key + "-label";
                 let inputId = "option-" + key;
+                
                 return (             
                     <span>
-                        <input type={optionType} id={inputId} name={inputId} value={o} />
+                        <input type={optionType} id={inputId} name={this.inputName} value={o} />
                         <label htmlFor={inputId} className={`${styles.option}`} id={labelId}>{o}</label>
                     </span>
                 );
@@ -68,12 +94,47 @@ export default class Questions extends React.Component<{}, IQuestionsDetail> {
             return questionOptionElements;
         }
         else {
-            return <input type="text" placeholder="Type your answer here" />
+            return <input type="text" id="txtAnswerFreeText" placeholder="Type your answer here" />
         }
     }
 
     private getQuestion(index: number) {
         return this.questions[index];
+    }
+
+    private renderNextQuestion(currentQuestion: any) {
+        let answered = false;
+        if(currentQuestion.hasOptions === true && currentQuestion.multipleChoices === true) {
+            let answersElement = document.getElementsByName(this.inputName);
+            for(var x=0; x <= answersElement.length; x++) {
+                let userAnswerChecked = (answersElement[x] as HTMLInputElement).checked;
+                if(userAnswerChecked === true) {
+                    currentQuestion.userAnswer.push((answersElement[x] as HTMLInputElement).value);
+                    answered = true;
+                }
+            }        
+        }
+        else if(currentQuestion.hasOptions === true && currentQuestion.multipleChoices === false) {
+            let answersElement = document.getElementsByName(this.inputName);
+            for(var x=0; x <= answersElement.length; x++) {
+                let userAnswerChecked = (answersElement[x] as HTMLInputElement).checked;
+                if(userAnswerChecked === true) {
+                    currentQuestion.userAnswer = (answersElement[x] as HTMLInputElement).value;
+                    answered = true;
+                }
+            }        
+        }
+        else {
+            currentQuestion.userAnswer = (document.getElementById("txtAnswerFreeText") as HTMLInputElement).value;
+            answered = true;
+        }
+
+        if(answered) {
+
+        }
+        else {
+            alert("Please answer the question first.")
+        }
     }
 
     private questions: any[] = [
